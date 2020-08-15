5b3fb13e02f5d1bd4140965d7a4792c7b9cae025
Rahul <rahul.27bok@gmail.com>
2020-08-16 03:19:15 +0530

    first commit

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
 
-//------------ Routes ------------//
-app.use('/', require('./routes/index'));
-app.use('/auth', require('./routes/users'));
-
 //------------ EJS Configuration ------------//
 app.use(expressLayouts);
 app.use("/assets", express.static('./assets'));
 app.set('view engine', 'ejs');
 
+//------------ Routes ------------//
+app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/users'));
 
 const PORT = process.env.PORT || 3006;
 
